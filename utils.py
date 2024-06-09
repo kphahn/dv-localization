57c750088642e4842e25aa8d9c9852eab9f7985e
Kjell-Philipp Hahn <kjell-philipp_hahn@web.de>
2024-06-09 17:00:54 +0200

    Cluster Point Groups
    - group points with dbscan algorithm
    - highlight each with different color
    - remove redundant utils code
    - update requirements
    - update notes and to dos

--- utils.py
+++ utils.py
@@ -1,59 +1,25 @@
-from argparse import ArgumentParser
 from pathlib import Path
-import plotly.graph_objects as go
-import plotly.io as pio
-import numpy as np
-import h5py
 import open3d as o3d
+import numpy as np
 
-pio.templates.default = "plotly_dark"
 
-def plot_point_cloud(file: Path, noise: bool = False):
+def generate_colors(num_clusters):
+    colors = []
+    np.random.seed(42)
+
+    for _ in range(num_clusters):
+        color = np.random.random(size=3)
+        colors.append(color)
+
+    return colors
+
+
+def plot_point_cloud_from_file(file: str, uniform_color: bool = True):
     file = Path(file)
     if not file.exists():
         raise FileNotFoundError(f"Error: {file} does not exist")
-    
-    suffix = file.suffix
 
-    if suffix == ".hdf5":
-        with h5py.File(file) as data:
-            x = data[f"location{'_noise' if noise else ''}_x"][()][0].astype(np.float32)
-            y = data[f"location{'_noise' if noise else ''}_y"][()][0].astype(np.float32)
-            z = data[f"location{'_noise' if noise else ''}_z"][()][0].astype(np.float32)
-            intensity = data["intensity"][()][0].astype(np.float32)
-
-        fig = go.Figure(
-            data=[
-                go.Scatter3d(
-                    x=x,
-                    y=y,
-                    z=z,
-                    mode="markers",
-                    marker=dict(
-                        size=3, color=intensity, colorscale="Viridis", opacity=0.8
-                    ),
-                )
-            ]
-        )
-
-        fig.update_layout(scene=dict(aspectmode="data"))
-        fig.show()
-    
-    if suffix == ".ply":
-        file = str(file)
-        if noise:
-            file = file.rpartition('.')[0] + "_noise" + suffix
-        pcd = o3d.io.read_point_cloud(file)
-
+    pcd = o3d.io.read_point_cloud(file)
+    if uniform_color:
         pcd = pcd.paint_uniform_color([0.5, 0.5, 0.5])
-        o3d.visualization.draw_geometries([pcd])
-
-def main():
-    parser = ArgumentParser()
-    parser.add_argument("file", type=Path, help="Path to the point cloud file")
-    parser.add_argument("--noise", action="store_true", help="Plot the noisy point cloud")
-    args = parser.parse_args()
-    plot_point_cloud(args.file, noise=args.noise)
-
-if __name__ == "__main__":
-    main()
+    o3d.visualization.draw_geometries([pcd])
