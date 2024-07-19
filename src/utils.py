d76a900c70250bf11d42ebd9b8866eeb24dfa9bf
Kjell-Philipp Hahn <kjell-philipp.hahn@student.htw-berlin.de>
2024-07-19 14:09:57 +0000

    Implementation of Pose Graph

--- src/utils.py
+++ src/utils.py
@@ -1,32 +1,25 @@
+import os
 import open3d as o3d
 import numpy as np
-import json
+import csv
+import copy
+
+from enum import Enum
+from tqdm import tqdm
 
 
-def generate_colors(num_clusters):
+### GENERIC ###
+
+
+def generate_colors(n):
     colors = []
-    np.random.seed(42)
+    np.random.seed(1)
 
-    for _ in range(num_clusters):
+    for _ in range(n):
         color = np.random.random(size=3)
         colors.append(color)
 
     return colors
-
-
-def load_ground_truth_from_json(file_path):
-    with open(file_path) as fp:
-        track_data = json.load(fp)
-
-    cone_positions = (
-        track_data["blue_cones"] + track_data["yellow_cones"] + track_data["big_cones"]
-    )
-    for cp in cone_positions:
-        cp.append(0)
-
-    cone_positions = o3d.utility.Vector3dVector(cone_positions)
-
-    return o3d.geometry.PointCloud(cone_positions)
 
 
 def draw(pointclouds):
@@ -35,3 +28,177 @@ def draw(pointclouds):
         show_skybox=False,
         point_size=5,
     )
+
+
+### FRAMES ###
+
+MIN_POINTS_FOR_DETECTION = 10
+FRAME_DIVIDER = 2
+
+NODE_TYPE = Enum("Node_Type", ["POSE", "LANDMARK"])
+EDGE_TYPE = Enum("Edge_Type", ["ODOMETRY", "OBSERVATION", "LOOP_CLOSURE"])
+
+
+def read_transform_from_csv(file_path):
+
+    with open(file_path, "r") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            transform = {
+                "x": float(row["x"]),
+                "y": float(row["y"]),
+                "z": float(row["z"]),
+                "rot_x": float(row["rot_x"]),
+                "rot_y": float(row["rot_y"]),
+                "rot_z": float(row["rot_z"]),
+            }
+
+    return transform
+
+
+def correct_frame(path_to_transform, frame):
+
+    # transform frame coodinate system to LiDAR coordinate system
+    transform = read_transform_from_csv(path_to_transform)
+    T = np.eye(4)
+    T[:3, :3] = o3d.geometry.PointCloud.get_rotation_matrix_from_axis_angle(
+        [
+            transform["rot_x"],
+            transform["rot_y"],
+            transform["rot_z"],
+        ]
+    )
+    T[:3, 3] = [transform["x"], transform["y"], transform["z"]]
+    T_inv = np.linalg.inv(T)
+
+    return frame.transform(T_inv)
+
+
+def load_dataset(dataset_path, limit=None):
+
+    if limit:
+        n = limit
+
+    else:
+        n = int(len(os.listdir(f"{dataset_path}/pointclouds")) / 2)
+    print(f"Loading Dataset with {n} pointclouds")  # exclude frames with noise
+
+    pcds = []
+
+    for i in tqdm(range(0, n, FRAME_DIVIDER)):
+
+        if os.path.exists(f"{dataset_path}/pointclouds/cloud_frame_{i}.ply"):
+            try:
+                frame = o3d.io.read_point_cloud(
+                    f"{dataset_path}/pointclouds/cloud_frame_{i}.ply"
+                )
+
+                frame = correct_frame(
+                    f"{dataset_path}/transformations/transformation_{i}.csv",
+                    frame,
+                )
+
+                pcds.append(frame)
+
+            except FileNotFoundError:
+                print(f"FileNotFoundError with frame {i}")
+
+        else:
+            break
+
+    return pcds
+
+
+def get_cone_positions(frame):
+
+    # remove ground plane
+    _, inliers = frame.segment_plane(
+        distance_threshold=0.01, ransac_n=3, num_iterations=100
+    )
+    frame = frame.select_by_index(inliers, invert=True)
+
+    # cluster remaining pointcloud and get cone centers
+    labels = np.array(
+        frame.cluster_dbscan(
+            eps=0.1, min_points=MIN_POINTS_FOR_DETECTION, print_progress=True
+        )
+    )
+    max_label = labels.max()
+
+    clusters = [
+        frame.select_by_index(np.where(labels == i)[0]) for i in range(max_label + 1)
+    ]
+
+    # derive cone positions from center
+    positions = o3d.utility.Vector3dVector(
+        [cluster.get_center() for cluster in clusters]
+    )
+    print()
+
+    # remove z-component
+    for position in positions:
+        position[2] = 0
+
+    return positions
+
+
+def preprocess_frame(frame):
+    return o3d.geometry.PointCloud(get_cone_positions(frame))
+
+
+def generate_static_visualization(
+    frames,
+    pose_graph,
+    node_types,
+    visualize_poses=False,
+    visualize_edges=True,
+    show_frames=[],
+):
+    frames = copy.deepcopy(frames)
+    poses = o3d.geometry.TriangleMesh.create_coordinate_frame()
+    edges = o3d.geometry.LineSet()
+    path = o3d.geometry.PointCloud()
+
+    i = 0
+    for idx, node in enumerate(pose_graph.nodes):
+        if node_types[idx] == NODE_TYPE.POSE:
+            frames[i].transform(node.pose)
+
+            if visualize_poses:
+                pose = o3d.geometry.TriangleMesh.create_coordinate_frame()
+                pose.transform(node.pose)
+                poses = poses + pose
+
+            if visualize_edges:
+                point = np.asarray(node.pose[:3, 3])
+                edges.points.append(point)
+
+                path.points.append(point)
+                path.colors.append([1, 0.65, 0])
+
+                if i > 0:
+                    edges.lines.append([i - 1, i])
+                    edges.colors.append([0, 0, 0])
+
+                if i == len(frames) - 1:
+                    edges.lines.append([i, 0])
+                    edges.colors.append([1, 0, 0])
+
+            i += 1
+
+    pointcloud = []
+
+    if len(show_frames):
+        for n in show_frames:
+            pointcloud.append(frames[n])
+    else:
+        pointcloud = frames
+
+    if visualize_poses:
+        pointcloud.append(poses)
+
+    if visualize_edges:
+        pointcloud.append(edges)
+        pointcloud.append(path)
+
+    return pointcloud
